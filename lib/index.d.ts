a8390aaf62c09aeb495c544fa79cd4075dc5ca3d
Astarosh <Astarosh@avansoma.no>
2021-10-25 15:50:08 +0300

    init

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export * from './generated/schema';
+export * from './generated';
+export * from './generated/index.js';
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './generated/schema'
+export * from './generated'
+export * from './generated/index.js'
+
+export * from './interfaces/enums'
+//export * from './interfaces/jwtClaims'
